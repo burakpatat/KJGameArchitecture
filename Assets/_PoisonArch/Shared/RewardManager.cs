cd1520566e68ca8f0f1bfce22d34130facd647e4
Burak PATAT <poisonsoftwaredevelop@gmail.com>
2023-01-25 11:50:17 +0300

    send

--- Assets/_PoisonArch/Shared/RewardManager.cs
+++ Assets/_PoisonArch/Shared/RewardManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PoisonArch;
 
-public class RewardManager : MonoBehaviour
+using TMPro;
+using DG.Tweening;
+using System;
+
+public enum StarCount_e
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    OneStar = 1,
+    TwoStar,
+    ThreeStar
+}
+public class RewardManager : AbstractSingleton<AudioManager>
+{
+    [Header("Star")]
+    public List<Transform> Stars;
+    private StarCount_e StarCountEnum;
+    public TMP_Text StarText;
 
-    // Update is called once per frame
+    [Header("Coin & Gem")]
+    public TMP_Text CoinCountText;
+    public TMP_Text GemCountText;
+
     void Update()
     {
-        
+        GetStar(StarCount_e.TwoStar);
+    }
+    public void GetStar(Enum _enum)
+    {
+        StarCountEnum = (StarCount_e)_enum;
+        int _openCount = (int)Enum.ToObject(StarCountEnum.GetType(), StarCountEnum);
+
+        for (int i = 0; i < _openCount; i++)
+        {
+            Stars[i].transform.GetChild(1).gameObject.SetActive(true);
+        }
+
+        switch (StarCountEnum)
+        {
+            case StarCount_e.OneStar:
+                StarText.text = "TEBRÝKLER!";
+                break;
+            case StarCount_e.TwoStar:
+                StarText.text = "BRAVO!";
+                break;
+            case StarCount_e.ThreeStar:
+                StarText.text = "HARÝKASIN!";
+                break;
+
+        }
     }
 }
